9dc17797d3203faca14fa182a1c06b359a3b8f54
john-jaihyek-choi <jaihyek.choi@gmail.com>
2020-07-03 00:40:34 -0700

    added image on page load and food is visible

--- snake.js
+++ snake.js
@@ -1,3 +1,8 @@
-const board = document.querySelector('board');
+const board = document.getElementById('board');
 const ctx = board.getContext('2d');
 
+let foodImage = new Image();
+foodImage.src = './asset/images/apple.png';
+foodImage.onload = () => {
+    ctx.drawImage(foodImage, 10, 10, 50, 50);
+}
